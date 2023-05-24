c02610467ca7f49c22638c24a848d710d6d348e2
Bruno-mateus <bruno_205@hotmail.com>
2023-05-24 12:01:21 -0300

    intial screen and api config

---
+++ src/api/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  method: 'get',
+  url: 'https://v3.football.api-sports.io/{endpoint}',
+  headers: {
+    'x-rapidapi-key': 'XxXxXxXxXxXxXxXxXxXxXxXx',
+    'x-rapidapi-host': 'v3.football.api-sports.io'
+  }
+};
+
+
+
